738cfc45d0702883c629aff1d6c841b90f882458
Lisa <ljenkin2@hotmail.com>
2012-11-23 12:28:07 -0500

    Initial Commit

---
+++ Gone/GoneAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GoneAppDelegate.h
+//  Gone
+//
+//  Created by Lisa Jenkins on 11/23/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoneAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gone
+//
+//  Created by Lisa Jenkins on 11/23/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GoneAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GoneAppDelegate class]));
+    }
+}
